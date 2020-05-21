7250cb63a9ef60a742fe5380af4013c3c8ac529f
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2020-05-21 02:04:24 -0300

    dia e mes ok
    falta ano

--- numero_extenso.py
+++ numero_extenso.py
@@ -1,28 +1,30 @@
 lista1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,30,"de"]
-lista2=["zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","vinte","trinta","de"]
+lista2=["","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","vinte","trinta","de"]
 meses=["","janeiro","fevereiro","marco","abril","maio","junho","julho","agosto","setembro","outrubro","novembro","dezembro"]
 x=input("digite um numero: ")
 x=x.replace(" "," de ")
-print(x)
 listaX=x.split()
 print(listaX)
 
 # mes
 mes=int(listaX[2])#transforma o mes em inteiro
-print(type(mes))
 if (mes<=12):
     listaX[2]=(meses[mes])
-
-print(listaX)
+else:
+    print("ERRO")
 
 #dia
 dia=int(listaX[0])#transforma o dia em inteiro
-if dia not in lista1:
-    d=dia
-    dezena=dia//10
-    unidade=dia%10
+dezena=dia//10
+unidade=dia%10
+
+if dia<=31:
+
+    if dezena == 0:
+        listaX[0]=lista2[dia]
+
     if dezena == 1:
-        if unidade == 8:
+        if (unidade == 8) or (unidade == 0):
             dezena=lista2[10]
             unidade=lista2[unidade]
             dia =(dezena+unidade)
@@ -31,15 +33,40 @@ if dia not in lista1:
             dezena=("dezes")
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia    
-        else:
-            print("ta aqui")
-            dezena=("dezes")
+            listaX[0]=dia
+        elif (unidade == 9):
+            dezena=("deze")
             unidade=lista2[unidade]
             dia =(dezena+unidade)
             listaX[0]=dia
+        else:            
+            listaX[0]=lista2[dia]
         
-print(listaX)
+    if dezena == 2:
+        if (unidade == 0):
+            dezena=lista2[16]
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            listaX[0]=dia        
+        else:
+            dezena=("vinte e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            listaX[0]=dia
+
+    if dezena == 3:
+        if (unidade == 0):
+            listaX[0]=lista2[17]        
+        else:
+            dezena=("trinta e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            listaX[0]=dia
+else:
+    print("ERROR")
+
+    
+
 datatr=str(listaX).strip("[]")#retira os conchetes
 datatr=datatr.replace("'","")#retira as aspas
 datatr=datatr.replace(",","")#retira as virgulas
