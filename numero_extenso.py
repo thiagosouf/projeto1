8ee35594e5974350948a11b594d82d615f2d3deb
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2020-05-23 13:27:27 -0300

    dia,mes,ano ok

--- numero_extenso.py
+++ numero_extenso.py
@@ -1,84 +1,179 @@
-listadez=["quarenta","cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
-listacent=["cem","cento", "duzentos", "trezentos", "centos", "mil"]
-lista2=["","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","vinte","trinta","de"]
-meses=["","janeiro","fevereiro","marco","abril","maio","junho","julho","agosto","setembro","outrubro","novembro","dezembro"]
-x=input("digite um numero: ")
-x=x.replace(" "," de ")
-listaX=x.split()
-print(listaX)
+#ano
+def anoX(ano):
+    milhar=ano//1000
+    a=ano%1000
+    centena=a//100
+    a=ano%100
+    dezena=a//10
+    unidade=ano%10
+    dia=ano%100
+    if milhar == 1:
+        if (centena==0) and (dezena==0) and (unidade==0):
+            m=lista2[26] #mil
+            ano=(m)
+            return(ano) 
+        if (centena==9) and (dezena==0) and (unidade==0):
+            m=lista2[26] #mil
+            u=lista2[9] #nove
+            c=lista2[25] #centos
+            ano=(m+" e "+u+c)
+            return(ano) #mil/nove/centos
+        else:
+            if (centena==9):
+                m=lista2[26] #mil
+                u=lista2[9] #nove
+                c=lista2[25] #centos
+                d=diaX(dia)
+                ano=(m+" "+u+c+" e "+d)
+                return(ano)
 
-# mes
-mes=int(listaX[2])#transforma o mes em inteiro
-if (mes<=12):
-    listaX[2]=(meses[mes])
-else:
-    print("ERRO")
+    if milhar == 2:
+        if (centena==0) and (dezena==0) and (unidade==0):
+            u=lista2[2] #dois
+            m=lista2[26] #mil
+            ano=(u+" "+m)
+            return(ano) 
+        if (centena==1) and (dezena==0) and (unidade==0):
+            u=lista2[2] #dois
+            m=lista2[26] #mil
+            c=lista2[24] #cem
+            ano=(u+" "+m+" e "+c)
+            return(ano) 
+        else:
+            if (centena==0):
+                u=lista2[2] #dois
+                m=lista2[26] #mil
+                d=diaX(dia)
+                ano=(u+" "+m+" e "+d)
+                return(ano) 
+
 
 #dia
-dia=int(listaX[0])#transforma o dia em inteiro
-dezena=dia//10
-unidade=dia%10
-
-if dia<=31:
+def diaX(dia):
+    dezena=dia//10
+    unidade=dia%10
 
     if dezena == 0:
-        listaX[0]=lista2[dia]
+        return(lista2[dia])
 
     if dezena == 1:
         if (unidade == 8) or (unidade == 0):
             dezena=lista2[10]
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia
+            return(dia)
         elif (unidade == 6) or (unidade == 7):
             dezena=("dezes")
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia
+            return(dia)
         elif (unidade == 9):
             dezena=("deze")
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia
+            return(dia)
         else:            
-            listaX[0]=lista2[dia]
-        
+            return(lista2[dia])
+           
     if dezena == 2:
         if (unidade == 0):
             dezena=lista2[16]
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia        
+            return(dia)        
         else:
             dezena=("vinte e ")
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia
+            return(dia)
 
     if dezena == 3:
         if (unidade == 0):
-            listaX[0]=lista2[17]        
+            return(lista2[17])        
         else:
             dezena=("trinta e ")
             unidade=lista2[unidade]
             dia =(dezena+unidade)
-            listaX[0]=dia
-else:
-    print("ERROR")
+            return(dia)
 
-    
-
-datatr=str(listaX).strip("[]")#retira os conchetes
-datatr=datatr.replace("'","")#retira as aspas
-datatr=datatr.replace(",","")#retira as virgulas
-print(datatr)
+    if dezena == 4:
+        if (unidade == 0):
+            return(lista2[18])        
+        else:
+            dezena=(lista2[18]+" e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            return(dia)
         
+    if dezena == 5:
+        if (unidade == 0):
+            return(lista2[19])        
+        else:
+            dezena=(lista2[19]+" e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            return(dia)
+        
+    if dezena == 6:
+        if (unidade == 0):
+            return(lista2[20])        
+        else:
+            dezena=(lista2[20]+" e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            return(dia)
+        
+    if dezena == 7:
+        if (unidade == 0):
+            return(lista2[21])        
+        else:
+            dezena=(lista2[21]+" e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            return(dia)
+
+    if dezena == 8:
+        if (unidade == 0):
+            return(lista2[22])        
+        else:
+            dezena=(lista2[22]+" e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            return(dia)
+        
+    if dezena == 9:
+        if (unidade == 0):
+            return(lista2[23])        
+        else:
+            dezena=(lista2[23]+" e ")
+            unidade=lista2[unidade]
+            dia =(dezena+unidade)
+            return(dia)
     
-#for d in listaX:
-#    if d == ("de"):
-#        print ("de",end=" ")
-#    else:
-#        for e in lista1:
-#            d=int(d)
-#           if e == d:
-#                print(lista2[d],end=" ")
+#mes
+def mesX(mes):
+    return (meses[mes])
+    
+lista2=["","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","vinte","trinta",
+        "quarenta","cinquenta", "sessenta", "setenta", "oitenta", "noventa","cem","centos","mil","cento"]
+meses=["","janeiro","fevereiro","marco","abril","maio","junho","julho","agosto","setembro","outrubro","novembro","dezembro"]
+x=input("digite um numero: ")
+x=x.replace(" "," de ")
+listaX=x.split()
+dia=int(listaX[0]) #transforma o dia em inteiro
+mes=int(listaX[2]) #transforma o mes em inteiro
+ano=int(listaX[4]) #transforma o ano em inteiro
+if (dia<=31) and (mes<=12) and (ano>=1900) and (ano<2100):
+    listaX[0]=diaX(dia)
+    listaX[2]=mesX(mes)
+    listaX[4]=anoX(ano)
+
+    datatr=str(listaX).strip("[]")#retira os conchetes
+    datatr=datatr.replace("'","")#retira as aspas
+    datatr=datatr.replace(",","")#retira as virgulas
+    print(datatr)
+else:
+    print("ERRO")
+
+
+
